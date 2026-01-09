c70bdbfe37ba988ee5a0a92eaf116e2cbcf3dd4c
kathiresan-chiz <kathirresan999@gmail.com>
2026-01-09 16:05:59 +0530

    Comment out authRoutes middleware
    Comment out the authRoutes middleware for future reference.

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 // app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/auth", authRoutes);
+// app.use("/auth", authRoutes);
     await api.post("/auth/forgot-password", { email });
 
 
