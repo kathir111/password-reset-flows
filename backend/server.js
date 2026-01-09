40c1f5b20cdddbe51783d6992e5b642ad3c6f920
kathiresan-chiz <kathirresan999@gmail.com>
2026-01-09 16:01:48 +0530

    Update authentication route to '/auth'

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use(cors({
 app.use(express.json());
 
 // app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/auth", authRoutes);
     await api.post("/auth/forgot-password", { email });
 
 
